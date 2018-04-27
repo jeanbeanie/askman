52da23de56ad1001a0260f56fcba79fc428586e0
jean <msjeanneramos@gmail.com>
2018-04-27 19:16:19 -0400

    added a few more input placeholders and updated the positioning of these inputs to match the style I want

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -16,7 +16,7 @@ class HomeContainer extends React.Component <{}, {}> {
 
   loadInitialData() {
     const promises = [];
-    promises.push(this.props.loadInitialData())
+    promises.push(this.props.loadInitialData());
     Promise.all(promises).then((data) => {
       this.setState(data[0]);
     }).catch(err => console.log('Error loading data in HomeContainer', err));
@@ -30,26 +30,65 @@ class HomeContainer extends React.Component <{}, {}> {
   render() {
     return (
       <div className="text-center">
-        <h1 id="headline">AskManager</h1>
+        <h1 id="headline"><span className="badge-pill title-badge">AskManager</span></h1>
         <strong id="subheadline">Set It and Forget It Asking Via SMS!</strong>
         <hr />
         <form onSubmit={this.handleSubmit}>
-          <label>
-            Task
-            <input className="form-control" type="text" defaultValue="take out the trash" />
-          </label>
-          <br />
-          <label>
-            Due Date
-            <input className="form-control" type="date" />
-          </label>
-          <br />
-          <label>
-            Phone Number (10 Digit)
-            <input className="form-control" defaultValue="15555555555" type="tel" size="10"/>
-          </label>
-          <br />
-          <input type="submit" className="btn btn-lg" value="Submit Ask" />
+          <div className="row">
+            <div className="col-sm form-group">
+              <label>
+              Task
+                <input className="form-control" type="text" defaultValue="take out the trash" />
+              </label>
+            </div>
+
+            <div className="col-sm form-group">
+              <label>
+              Due Date
+                <input className="form-control" type="date" />
+              </label>
+            </div>
+
+          </div>
+          <div className="row">
+            <div className="col-sm form-group">
+              <label>
+              Possible Names
+                <input className="form-control" defaultValue="babe, baby, bae, boo, love" type="text" />
+              </label>
+            </div>
+          </div>
+          <div className="row">
+
+            <div className="col-sm form-group">
+              <label>
+              Possible Intros
+                <input className="form-control" defaultValue="don't forget to, oh yea can you, I need a favor can you " type="text" />
+              </label>
+            </div>
+          </div>
+          <div className="row">
+
+            <div className="col-sm form-group">
+              <label>
+              Possible Endings
+                <input className="form-control" defaultValue="thank you, thx so much, thanks, I appreciate it" type="text" />
+              </label>
+            </div>
+
+
+          </div>
+
+          <div className="row">
+            <div className="col-sm form-group">
+              <label>
+              Phone Number (10 Digit)
+                <input className="form-control" defaultValue="15555555555" type="tel" size="10" />
+              </label>
+              <input type="submit" className="btn btn-lg" value="Submit Ask" />
+            </div>
+
+          </div>
         </form>
       </div>
     );
