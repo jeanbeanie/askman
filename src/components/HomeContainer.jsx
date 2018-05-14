c822b25e93c1ac235ee1a18edcbbe883a0569a5b
jean <msjeanneramos@gmail.com>
2018-05-14 14:14:19 -0400

    added new components to break up the one large one, TaskForm, RandomText, and DatePicker get props passed down from the HomeContainer. I also added flowtypes for the new components

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -3,6 +3,147 @@
 
 import React from 'react';
 
+const RandomText = (props) => {
+  return (
+    <div>
+      <h1 id="headline"><span className="badge-pill title-badge">AskManager</span></h1>
+      <strong id="subheadline">Set It and Forget It Asking Via SMS!</strong>
+      <hr />
+      <div className="row">
+        {/* Example randomly generated SMS text using form fields */}
+        <h6>
+          <strong id="subheadline">Example Random Text : </strong> {props.randomText}
+        </h6>
+        <button className="btn input" onClick={props.generateText}>GENERATE</button>
+      </div>
+      <hr />
+    </div>
+  )
+}
+
+type DatePickerProps = {
+  onChange: ()=> void,
+  taskIsRepeatable: boolean,
+}
+
+const DatePicker = (props: DatePickerProps) => {
+  const weekdayList = ['sun', 'mon', 'tues', 'wed', 'thurs', 'fri', 'sat'];
+
+  // map over weekdayList and return a list of label/inputs for selecting weekdays
+  const weekdayInputs = weekdayList.map((day, i) => (
+    <label htmlFor={`taskWeekdays['${i}']`} key={`taskWeekdays['${day}']`}>
+      {day[0].toUpperCase()} <input className="form-check-input" onChange={props.onChange} name="taskWeekdays" id={i} type="checkbox" />
+    </label>
+  ));
+
+  // form element for selecting days of the week using the weekdayInputs
+  const weekdayPicker = (
+    <div className="col-sm form-group">
+      <label htmlFor="weekdayPicker">
+        Weekdays
+        <div id="weekdayPicker" className="col-sm form-check form-check-inline input">
+          {weekdayInputs}
+        </div>
+      </label>
+    </div>
+  );
+
+  // select the date task must be completed by using the browser's built in datepicker
+  const dayPicker = (
+    <div className="col-sm form-group">
+      <label htmlFor="taskDueDate">
+        Due Date
+        {/* TODO : valid date should be today and future only */}
+        <input onChange={props.onChange} name="taskDueDate" className="form-control" id="taskDueDate" type="date" />
+      </label>
+    </div>
+  );
+
+  // if taskIsRepeatable render weekday picker else render single date picker
+  return props.taskIsRepeatable ? (weekdayPicker) : (dayPicker);
+};
+
+type TaskFormProps = {
+  onSubmit: ()=> void,
+  onChange: ()=> void,
+  datePicker: {},
+  taskBody: string,
+  taskDueTime: string,
+  taskIntros: string,
+  taskEndings: string,
+  phoneNumber: string,
+  recipientNames: string,
+}
+
+const TaskForm = (props: TaskFormProps) => {
+  return (
+    <form onSubmit={props.onSubmit} >
+
+      <div className="row">
+        {/* Subject body of task to be submitted */}
+        <div className="col-sm form-group">
+          <label htmlFor="taskBody">
+          Task
+            <input className="form-control" id="taskBody" name="taskBody" defaultValue={props.taskBody} onChange={props.onChange} type="text" />
+          </label>
+        </div>
+
+        {/* Weekly repeatable or single date picker */}
+        {props.datePicker}
+
+        {/* Select for time task should be started */}
+        <div className="col-sm form-group">
+          <label htmlFor="taskDueTime">
+          Due Time
+            <input className="form-control" id="taskDueTime" name="taskDueTime" defaultValue={props.taskDueTime} onChange={props.onChange} type="time" />
+          </label>
+        </div>
+      </div>
+
+      <div className="row">
+        {/* Intros to choose from for task text */}
+        <div className="col-sm form-group">
+          <label htmlFor="taskIntros">
+          Intros
+            <input className="form-control" name="taskIntros" id="taskIntros" onChange={props.onChange} defaultValue={props.taskIntros} type="text" />
+          </label>
+        </div>
+      </div>
+
+      <div className="row">
+        {/* Endings to choose from for task text */}
+        <div className="col-sm form-group">
+          <label htmlFor="taskEndings">
+          Endings
+            <input className="form-control" name="taskEndings" id="taskEndings" onChange={props.onChange} defaultValue={props.taskEndings} type="text" />
+          </label>
+        </div>
+      </div>
+
+      <div className="row">
+        {/* Phone number to send task text to */}
+        <div className="col-sm form-group">
+          <label htmlFor="phoneNumber">
+          Phone Number (10 Digit)
+            <input className="form-control" name="phoneNumber" id="phoneNumber" defaultValue={props.phoneNumber} onChange={props.onChange} type="tel" size="10" />
+          </label>
+        </div>
+
+        {/* Names to be chosen from for use in task text */}
+        <div className="col-sm form-group">
+          <label htmlFor="recipientNames">
+          Names
+            <input className="form-control" name="recipientNames" id="recipientNames" defaultValue={props.recipientNames} onChange={props.onChange} type="text" />
+          </label>
+        </div>
+      </div>
+
+      {/* FORM SUBMIT */}
+      <input type="submit" className="btn" value="SUBMIT ASK" />
+    </form>
+  )
+}
+
 type Props = {
   loadInitialData: ()=>{},
 };
@@ -32,7 +173,7 @@ class HomeContainer extends React.Component <Props, State> {
       recipientNames: 'babe, baby, bae, boo, love',
       taskIntros: 'don\'t forget to, oh yea can you, I need a favor can you',
       taskEndings: 'thank you, thx so much, thanks, I appreciate it',
-      taskIsRepeatable: false,
+      taskIsRepeatable: true,
       taskWeekdays: Array(7).fill(false),
     };
   }
@@ -42,7 +183,7 @@ class HomeContainer extends React.Component <Props, State> {
     this.generateRandomText();
   }
 
-  generateRandomText = () => {
+  generateRandomText = (): void => {
     // create func for returning random int based on arr length
     const randInt = x => Math.floor(Math.random() * Math.floor(x));
 
@@ -93,135 +234,37 @@ class HomeContainer extends React.Component <Props, State> {
     }
 
     // set the state property that matches the named element to the updated value
-    this.generateRandomText();
     this.setState({
       [name]: value,
     });
   }
 
-  // when form is submitted for now display an alert with components state for testing purposes
+  // display state in console for testing purposes
   handleSubmit = (event) => {
-    this.generateRandomText();
     console.log('State: ', this.state);
     event.preventDefault();
   }
 
   render() {
-    const weekdayList = ['sun', 'mon', 'tues', 'wed', 'thurs', 'fri', 'sat'];
-
-    // map over weekdayList and return a list of label/inputs for selecting weekdays
-    const weekdayInputs = weekdayList.map((day, i) => (
-      <label htmlFor={`taskWeekdays['${i}']`} key={`taskWeekdays['${day}']`}>
-        {day[0].toUpperCase()} <input className="form-check-input" onChange={this.handleChange} name="taskWeekdays" id={i} type="checkbox" />
-      </label>
-    ));
-
-    // form element for selecting days of the week using the weekdayInputs
-    const weekdayPicker = (
-      <div className="col-sm form-group">
-        <label htmlFor="weekdayPicker">
-          Weekdays
-          <div id="weekdayPicker" className="col-sm form-check form-check-inline input">
-            {weekdayInputs}
-          </div>
-        </label>
-      </div>
-    );
-
-    // select the date task must be completed by using the browser's built in datepicker
-    const dayPicker = (
-      <div className="col-sm form-group">
-        <label htmlFor="taskDueDate">
-          Due Date
-          {/* TODO : valid date should be today and future only */}
-          <input onChange={this.handleChange} name="taskDueDate" className="form-control" id="taskDueDate" type="date" />
-        </label>
-      </div>
-    );
-
-    // if taskIsRepeatable render weekday picker else render single date picker
-    const dateOptions = this.state.taskIsRepeatable ? (weekdayPicker) : (dayPicker);
-
     return (
       <div className="text-center">
 
-        <h1 id="headline"><span className="badge-pill title-badge">AskManager</span></h1>
-        <strong id="subheadline">Set It and Forget It Asking Via SMS!</strong>
-        <hr />
+        <RandomText
+          randomText={this.state.randomText}
+          generateText={this.generateRandomText}
+        />
 
-        <form onSubmit={this.handleSubmit}>
+        <TaskForm
+          {...this.state}
+          datePicker={
+            <DatePicker
+              onChange={this.handleChange}
+              taskIsRepeatable={this.state.taskIsRepeatable}
+            />}
+          onSubmit={this.handleSubmit}
+          onChange={this.handleChange}
+        />
 
-          <div className="row">
-            {/* Example randomly generated SMS text using form fields*/}
-            <h6>
-              <strong id="subheadline">Example Random Text : </strong> {this.state.randomText}
-            </h6>
-            <button className="btn input" onClick={this.generateRandomText}>GENERATE</button>
-          </div>
-          <hr />
-
-          <div className="row">
-            {/* Subject body of task to be submitted */}
-            <div className="col-sm form-group">
-              <label htmlFor="taskBody">
-              Task
-                <input className="form-control" id="taskBody" name="taskBody" defaultValue={this.state.taskBody} onChange={this.handleChange} type="text" />
-              </label>
-            </div>
-
-            {/* Weekly repeatable or single date picker */}
-            {dateOptions}
-
-            {/* Select for time task should be started */}
-            <div className="col-sm form-group">
-              <label htmlFor="taskDueTime">
-              Due Time
-                <input className="form-control" id="taskDueTime" name="taskDueTime" defaultValue={this.state.taskDueTime} onChange={this.handleChange} type="time" />
-              </label>
-            </div>
-          </div>
-
-          <div className="row">
-            {/* Intros to choose from for task text */}
-            <div className="col-sm form-group">
-              <label htmlFor="taskIntros">
-              Intros
-                <input className="form-control" name="taskIntros" id="taskIntros" onChange={this.handleChange} defaultValue={this.state.taskIntros} type="text" />
-              </label>
-            </div>
-          </div>
-
-          <div className="row">
-            {/* Endings to choose from for task text */}
-            <div className="col-sm form-group">
-              <label htmlFor="taskEndings">
-              Endings
-                <input className="form-control" name="taskEndings" id="taskEndings" onChange={this.handleChange} defaultValue={this.state.taskEndings} type="text" />
-              </label>
-            </div>
-          </div>
-
-          <div className="row">
-            {/* Phone number to send task text to */}
-            <div className="col-sm form-group">
-              <label htmlFor="phoneNumber">
-              Phone Number (10 Digit)
-                <input className="form-control" name="phoneNumber" id="phoneNumber" defaultValue={this.state.phoneNumber} onChange={this.handleChange} type="tel" size="10" />
-              </label>
-            </div>
-
-            {/* Names to be chosen from for use in task text */}
-            <div className="col-sm form-group">
-              <label htmlFor="recipientNames">
-              Names
-                <input className="form-control" name="recipientNames" id="recipientNames" defaultValue={this.state.recipientNames} onChange={this.handleChange} type="text" />
-              </label>
-            </div>
-          </div>
-
-          {/* FORM SUBMIT */}
-          <input type="submit" className="btn" value="SUBMIT ASK" />
-        </form>
       </div>
     );
   }
