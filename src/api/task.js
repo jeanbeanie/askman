7a495e756e97d29c226dd84f39b8a9d30282c508
jean <msjeanneramos@gmail.com>
2018-05-22 16:22:56 -0400

    added preliminary sendSMS method to Task class, is for now sending a hardcoded message to my own number, am now importing the twilio client package, added twilio account info in the noncommited config file

--- src/api/task.js
+++ src/api/task.js
@@ -1,4 +1,23 @@
+/* ./src/api/task.js */
+/* @flow */
+
+const {
+  accountSid, authToken, from, to,
+} = require('../config').twilio;
+const client = require('twilio')(accountSid, authToken);
+
 class Task {
+  // Send task ask through Twilio API
+  sendSMS() {
+    client.messages
+      .create({
+        body: 'The SMS body would be here',
+        from,
+        to,
+      })
+      .then(message => console.log(message.sid))
+      .done();
+  }
 }
 
 export default Task;
