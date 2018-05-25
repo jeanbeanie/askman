ec121ac8c88c8633bcba7206640ee05ef7889fd6
jean <msjeanneramos@gmail.com>
2018-05-24 21:21:03 -0400

    moved the code that was inside of the openMongo... function and moved it to the outer scope of the file. Now the task db related functions will share one collection, this is preferable to opening a new connection to the db each time a func is run.

--- src/api/task.js
+++ src/api/task.js
@@ -1,7 +1,7 @@
 /* ./src/api/task.js */
 /* @flow */
 
-// Twilio configuration
+/* Twilio configuration */
 const {
   accountSid,
   authToken,
@@ -10,47 +10,33 @@ const {
 } = require('../config').twilio;
 const client = require('twilio')(accountSid, authToken);
 
-// Mongo configuration
+/* Mongo configuration */
 const { MongoClient } = require('mongodb');
 const { url, name: dbName } = require('../config').db;
-
-// opens MongoDB connection and returns db & collection
-const openMongoGetDBCollection = (callback) => {
-  // connect to db
-  MongoClient.connect(url, (err, db) => {
-    if (err != null) { console.log('Error connecting to db', err) }
-    // grab 'Askman' database
-    const dbo = db.db(dbName);
-    // grab and return 'tasks' collection
-    dbo.collection('tasks', (err2, collection) => {
-      if (err2 != null) { console.log('Error retrieving collection', err2) }
-      const obj = { db, collection };
-      // pass db and collection to callback
-      callback(obj);
-    });
+// Create empty obj to hold mongo db and collection for use in funcs
+const mongo = {};
+// connect to db
+MongoClient.connect(url, (err, db) => {
+  if (err != null) { console.log('Error connecting to db', err) }
+  // grab 'Askman' database
+  const dbo = db.db(dbName);
+  // grab and return 'tasks' collection
+  dbo.collection('tasks', (err2, collection) => {
+    if (err2 != null) { console.log('Error retrieving collection', err2) }
+    mongo.collection = collection;
   });
-};
-
-// close MongoDB connection
-const closeMongo = (db) => {
-  db.close();
-};
+});
 
 
 /* PUBLIC TASK FUNCTIONS */
-
 module.exports = {
   // Get all tasks from MongoDB tasks collection
-  // callback should be a function that takes an array
   getAll: (callback: ([])=>{}) => {
-    openMongoGetDBCollection(({ db, collection }) => {
-      // grab everything from collection (no filter)
-      collection.find().toArray((err, list) => {
-        console.log('Retrieved all tasks in DB.');
-        // pass tasks to callback func then close DB
-        callback(list);
-        closeMongo(db);
-      });
+    // grab everything from collection (no filter)
+    mongo.collection.find().toArray((err, list) => {
+      console.log('Retrieved all tasks in DB.');
+      // pass tasks to callback func then close DB
+      callback(list);
     });
   },
 
